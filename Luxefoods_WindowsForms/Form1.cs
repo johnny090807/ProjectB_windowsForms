6108c7816f3c8a445dcea7d1e1c56a373c3ebe05
Kacper Kucharski <kacpikuch1@icloud.com>
2020-04-09 17:42:58 +0200

    Added Template Borderless Design

---
+++ Luxefoods_WindowsForms/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Luxefoods_WindowsForms
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void ExitButton_Click(object sender, EventArgs e)
+        {
+            if (System.Windows.Forms.Application.MessageLoop)
+            {
+                // WinForms app
+                System.Windows.Forms.Application.Exit();
+            }
+            else
+            {
+                // Console app
+                System.Environment.Exit(1);
+            }
+        }
+        private void ExitButton_Hover_Enter(object sender, EventArgs e)
+        {
+            ExitButton.ForeColor = Color.Red;
+        }
+        private void ExitButton_Hover_Leave(object sender, EventArgs e)
+        {
+            ExitButton.ForeColor = Color.Black;
+        }
+
+        private void MinimizeButton_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
+
+        private void MinimizeButton_Hover_Enter(object sender, EventArgs e)
+        {
+            MinimizeButton.ForeColor = Color.Red;
+        }
+
+        private void MinimizeButton_Hover_Leave(object sender, EventArgs e)
+        {
+            MinimizeButton.ForeColor = Color.Black;
+        }
+
+    }
+}
