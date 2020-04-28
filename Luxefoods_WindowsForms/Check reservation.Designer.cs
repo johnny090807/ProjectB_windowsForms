e8daa310c286a15739855f6f97d877a58c7121a1
RBLeon <57948143+RBLeon@users.noreply.github.com>
2020-04-28 16:20:49 +0200

    added the two front end screens

--- Luxefoods_WindowsForms/Check reservation.Designer.cs
+++ Luxefoods_WindowsForms/Check reservation.Designer.cs
@@ -32,6 +32,9 @@
             this.pictureBox2 = new System.Windows.Forms.PictureBox();
             this.ExitButton = new System.Windows.Forms.Label();
             this.MinimizeButton = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.Title = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             this.SuspendLayout();
             // 
@@ -72,12 +75,48 @@
             this.MinimizeButton.MouseEnter += new System.EventHandler(this.MinimizeButton_Hover_Enter);
             this.MinimizeButton.MouseLeave += new System.EventHandler(this.MinimizeButton_Hover_Leave);
             // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Black;
+            this.button2.ForeColor = System.Drawing.Color.White;
+            this.button2.Location = new System.Drawing.Point(746, 1081);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(427, 65);
+            this.button2.TabIndex = 14;
+            this.button2.Text = "Place Reservation";
+            this.button2.UseVisualStyleBackColor = false;
+            // 
+            // Title
+            // 
+            this.Title.BackColor = System.Drawing.Color.Black;
+            this.Title.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.Title.Font = new System.Drawing.Font("PMingLiU-ExtB", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Title.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.Title.Location = new System.Drawing.Point(550, 169);
+            this.Title.Name = "Title";
+            this.Title.Size = new System.Drawing.Size(804, 92);
+            this.Title.TabIndex = 18;
+            this.Title.Text = "Choose your reservation";
+            this.Title.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 46;
+            this.listBox1.Location = new System.Drawing.Point(186, 319);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(1551, 694);
+            this.listBox1.TabIndex = 19;
+            // 
             // reservation_template1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(23F, 46F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.Control;
             this.ClientSize = new System.Drawing.Size(1924, 1276);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.Title);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.MinimizeButton);
             this.Controls.Add(this.ExitButton);
             this.Controls.Add(this.pictureBox2);
@@ -96,5 +135,8 @@
         private System.Windows.Forms.PictureBox pictureBox2;
         private System.Windows.Forms.Label ExitButton;
         private System.Windows.Forms.Label MinimizeButton;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label Title;
+        private System.Windows.Forms.ListBox listBox1;
     }
 }
