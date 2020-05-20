fcd79c45735fd750319c57a73e6bd04c2b82acf1
RBLeon <57948143+RBLeon@users.noreply.github.com>
2020-05-20 12:25:27 +0200

    revert commit

--- Luxefoods_WindowsForms/Program.cs
+++ Luxefoods_WindowsForms/Program.cs
@@ -17,7 +17,7 @@ namespace Luxefoods_WindowsForms
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
 
-            Application.Run(new Reservation());
+            Application.Run(new Login());
         }
     }
 }
