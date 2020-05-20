fa35da71d19cf5bd267f1847eb056159dbcf7cbb
RBLeon <57948143+RBLeon@users.noreply.github.com>
2020-05-20 12:28:28 +0200

    Revert "Revert "aspect ratios 1920 1080""
    This reverts commit 5a545d366bcebd39b2727f392bdba07dbbebc887.

--- Luxefoods_WindowsForms/aboutUs.Designer.cs
+++ Luxefoods_WindowsForms/aboutUs.Designer.cs
@@ -29,9 +29,12 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(aboutUs));
-            this.pictureBox2 = new System.Windows.Forms.PictureBox();
             this.ExitButton = new System.Windows.Forms.Label();
             this.MinimizeButton = new System.Windows.Forms.Label();
+            this.HeaderLabel = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
             this.loginButton = new System.Windows.Forms.LinkLabel();
             this.contactUsButton = new System.Windows.Forms.LinkLabel();
             this.linkLabel1 = new System.Windows.Forms.LinkLabel();
@@ -39,27 +42,15 @@
             this.reservationsButton = new System.Windows.Forms.LinkLabel();
             this.aboutUsButton = new System.Windows.Forms.LinkLabel();
             this.LuxeFoodsLogoLabel = new System.Windows.Forms.Label();
-            this.HeaderLabel = new System.Windows.Forms.Label();
-            this.label1 = new System.Windows.Forms.Label();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             this.SuspendLayout();
-            // 
-            // pictureBox2
-            // 
-            this.pictureBox2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox2.BackgroundImage")));
-            this.pictureBox2.Location = new System.Drawing.Point(-1, -2);
-            this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(1925, 1282);
-            this.pictureBox2.TabIndex = 10;
-            this.pictureBox2.TabStop = false;
             // 
             // ExitButton
             // 
             this.ExitButton.AutoSize = true;
             this.ExitButton.Font = new System.Drawing.Font("Franklin Gothic Medium", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.ExitButton.Location = new System.Drawing.Point(1823, 30);
+            this.ExitButton.Location = new System.Drawing.Point(1822, 9);
             this.ExitButton.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.ExitButton.Name = "ExitButton";
             this.ExitButton.Size = new System.Drawing.Size(48, 51);
@@ -73,7 +64,7 @@
             // 
             this.MinimizeButton.AutoSize = true;
             this.MinimizeButton.Font = new System.Drawing.Font("Franklin Gothic Medium", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.MinimizeButton.Location = new System.Drawing.Point(1757, 30);
+            this.MinimizeButton.Location = new System.Drawing.Point(1754, 9);
             this.MinimizeButton.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.MinimizeButton.Name = "MinimizeButton";
             this.MinimizeButton.Size = new System.Drawing.Size(34, 51);
@@ -83,17 +74,59 @@
             this.MinimizeButton.MouseEnter += new System.EventHandler(this.MinimizeButton_Hover_Enter);
             this.MinimizeButton.MouseLeave += new System.EventHandler(this.MinimizeButton_Hover_Leave);
             // 
+            // HeaderLabel
+            // 
+            this.HeaderLabel.AutoSize = true;
+            this.HeaderLabel.BackColor = System.Drawing.Color.Black;
+            this.HeaderLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F);
+            this.HeaderLabel.ForeColor = System.Drawing.Color.White;
+            this.HeaderLabel.Location = new System.Drawing.Point(788, 135);
+            this.HeaderLabel.Name = "HeaderLabel";
+            this.HeaderLabel.Size = new System.Drawing.Size(318, 82);
+            this.HeaderLabel.TabIndex = 35;
+            this.HeaderLabel.Text = "About us";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Black;
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(268, 272);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(1421, 184);
+            this.label1.TabIndex = 36;
+            this.label1.Text = resources.GetString("label1.Text");
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(388, 513);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1193, 486);
+            this.pictureBox1.TabIndex = 37;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(1920, 1080);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 38;
+            this.pictureBox2.TabStop = false;
+            // 
             // loginButton
             // 
             this.loginButton.ActiveLinkColor = System.Drawing.Color.DarkGray;
             this.loginButton.AutoSize = true;
             this.loginButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(32)))), ((int)(((byte)(0)))));
-            this.loginButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.loginButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F);
             this.loginButton.LinkColor = System.Drawing.Color.White;
-            this.loginButton.Location = new System.Drawing.Point(1612, 42);
+            this.loginButton.Location = new System.Drawing.Point(1567, 24);
             this.loginButton.Name = "loginButton";
             this.loginButton.Size = new System.Drawing.Size(105, 40);
-            this.loginButton.TabIndex = 25;
+            this.loginButton.TabIndex = 45;
             this.loginButton.TabStop = true;
             this.loginButton.Text = "Login";
             this.loginButton.VisitedLinkColor = System.Drawing.Color.White;
@@ -103,12 +136,12 @@
             this.contactUsButton.ActiveLinkColor = System.Drawing.Color.DarkGray;
             this.contactUsButton.AutoSize = true;
             this.contactUsButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(32)))), ((int)(((byte)(0)))));
-            this.contactUsButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.contactUsButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F);
             this.contactUsButton.LinkColor = System.Drawing.Color.White;
-            this.contactUsButton.Location = new System.Drawing.Point(1400, 42);
+            this.contactUsButton.Location = new System.Drawing.Point(1345, 24);
             this.contactUsButton.Name = "contactUsButton";
             this.contactUsButton.Size = new System.Drawing.Size(189, 40);
-            this.contactUsButton.TabIndex = 24;
+            this.contactUsButton.TabIndex = 44;
             this.contactUsButton.TabStop = true;
             this.contactUsButton.Text = "Contact us";
             this.contactUsButton.VisitedLinkColor = System.Drawing.Color.White;
@@ -120,10 +153,10 @@
             this.linkLabel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(32)))), ((int)(((byte)(0)))));
             this.linkLabel1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F);
             this.linkLabel1.LinkColor = System.Drawing.Color.White;
-            this.linkLabel1.Location = new System.Drawing.Point(541, 41);
+            this.linkLabel1.Location = new System.Drawing.Point(500, 24);
             this.linkLabel1.Name = "linkLabel1";
             this.linkLabel1.Size = new System.Drawing.Size(271, 40);
-            this.linkLabel1.TabIndex = 22;
+            this.linkLabel1.TabIndex = 42;
             this.linkLabel1.TabStop = true;
             this.linkLabel1.Text = "My reservations";
             this.linkLabel1.VisitedLinkColor = System.Drawing.Color.White;
@@ -135,10 +168,10 @@
             this.menuButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(32)))), ((int)(((byte)(0)))));
             this.menuButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F);
             this.menuButton.LinkColor = System.Drawing.Color.White;
-            this.menuButton.Location = new System.Drawing.Point(397, 39);
+            this.menuButton.Location = new System.Drawing.Point(357, 24);
             this.menuButton.Name = "menuButton";
             this.menuButton.Size = new System.Drawing.Size(107, 40);
-            this.menuButton.TabIndex = 23;
+            this.menuButton.TabIndex = 43;
             this.menuButton.TabStop = true;
             this.menuButton.Text = "Menu";
             this.menuButton.VisitedLinkColor = System.Drawing.Color.White;
@@ -150,10 +183,10 @@
             this.reservationsButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(32)))), ((int)(((byte)(0)))));
             this.reservationsButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F);
             this.reservationsButton.LinkColor = System.Drawing.Color.White;
-            this.reservationsButton.Location = new System.Drawing.Point(846, 42);
+            this.reservationsButton.Location = new System.Drawing.Point(795, 24);
             this.reservationsButton.Name = "reservationsButton";
             this.reservationsButton.Size = new System.Drawing.Size(325, 40);
-            this.reservationsButton.TabIndex = 21;
+            this.reservationsButton.TabIndex = 41;
             this.reservationsButton.TabStop = true;
             this.reservationsButton.Text = "Place a reservation";
             this.reservationsButton.VisitedLinkColor = System.Drawing.Color.White;
@@ -165,10 +198,10 @@
             this.aboutUsButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(32)))), ((int)(((byte)(0)))));
             this.aboutUsButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F);
             this.aboutUsButton.LinkColor = System.Drawing.Color.White;
-            this.aboutUsButton.Location = new System.Drawing.Point(1207, 41);
+            this.aboutUsButton.Location = new System.Drawing.Point(1148, 24);
             this.aboutUsButton.Name = "aboutUsButton";
             this.aboutUsButton.Size = new System.Drawing.Size(159, 40);
-            this.aboutUsButton.TabIndex = 20;
+            this.aboutUsButton.TabIndex = 40;
             this.aboutUsButton.TabStop = true;
             this.aboutUsButton.Text = "About us";
             this.aboutUsButton.VisitedLinkColor = System.Drawing.Color.White;
@@ -180,79 +213,52 @@
             this.LuxeFoodsLogoLabel.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.LuxeFoodsLogoLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 26F);
             this.LuxeFoodsLogoLabel.ForeColor = System.Drawing.Color.White;
-            this.LuxeFoodsLogoLabel.Location = new System.Drawing.Point(36, 30);
+            this.LuxeFoodsLogoLabel.Location = new System.Drawing.Point(12, 9);
             this.LuxeFoodsLogoLabel.Name = "LuxeFoodsLogoLabel";
             this.LuxeFoodsLogoLabel.Size = new System.Drawing.Size(283, 59);
-            this.LuxeFoodsLogoLabel.TabIndex = 26;
+            this.LuxeFoodsLogoLabel.TabIndex = 39;
             this.LuxeFoodsLogoLabel.Text = "LuxeFoods";
-            // 
-            // HeaderLabel
-            // 
-            this.HeaderLabel.AutoSize = true;
-            this.HeaderLabel.BackColor = System.Drawing.Color.Black;
-            this.HeaderLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F);
-            this.HeaderLabel.ForeColor = System.Drawing.Color.White;
-            this.HeaderLabel.Location = new System.Drawing.Point(789, 175);
-            this.HeaderLabel.Name = "HeaderLabel";
-            this.HeaderLabel.Size = new System.Drawing.Size(318, 82);
-            this.HeaderLabel.TabIndex = 35;
-            this.HeaderLabel.Text = "About us";
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.BackColor = System.Drawing.Color.Black;
-            this.label1.ForeColor = System.Drawing.Color.White;
-            this.label1.Location = new System.Drawing.Point(269, 392);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(1421, 184);
-            this.label1.TabIndex = 36;
-            this.label1.Text = resources.GetString("label1.Text");
-            // 
-            // pictureBox1
-            // 
-            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(396, 648);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(1193, 486);
-            this.pictureBox1.TabIndex = 37;
-            this.pictureBox1.TabStop = false;
             // 
             // aboutUs
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(23F, 46F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.Control;
-            this.ClientSize = new System.Drawing.Size(1924, 1276);
-            this.Controls.Add(this.pictureBox1);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.HeaderLabel);
-            this.Controls.Add(this.LuxeFoodsLogoLabel);
+            this.ClientSize = new System.Drawing.Size(1920, 1080);
             this.Controls.Add(this.loginButton);
             this.Controls.Add(this.contactUsButton);
             this.Controls.Add(this.linkLabel1);
             this.Controls.Add(this.menuButton);
             this.Controls.Add(this.reservationsButton);
             this.Controls.Add(this.aboutUsButton);
+            this.Controls.Add(this.LuxeFoodsLogoLabel);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.HeaderLabel);
             this.Controls.Add(this.MinimizeButton);
             this.Controls.Add(this.ExitButton);
             this.Controls.Add(this.pictureBox2);
             this.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Margin = new System.Windows.Forms.Padding(8, 6, 8, 6);
+            this.MaximumSize = new System.Drawing.Size(1920, 1080);
+            this.MinimumSize = new System.Drawing.Size(1920, 1080);
             this.Name = "aboutUs";
             this.Text = "Make a aboutUs";
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
         }
 
         #endregion
-        private System.Windows.Forms.PictureBox pictureBox2;
         private System.Windows.Forms.Label ExitButton;
         private System.Windows.Forms.Label MinimizeButton;
+        private System.Windows.Forms.Label HeaderLabel;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
         private System.Windows.Forms.LinkLabel loginButton;
         private System.Windows.Forms.LinkLabel contactUsButton;
         private System.Windows.Forms.LinkLabel linkLabel1;
@@ -260,8 +266,5 @@
         private System.Windows.Forms.LinkLabel reservationsButton;
         private System.Windows.Forms.LinkLabel aboutUsButton;
         private System.Windows.Forms.Label LuxeFoodsLogoLabel;
-        private System.Windows.Forms.Label HeaderLabel;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
